4124df2d42dc883dc41e4c85cb75e8a9af71c35e
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2022-08-15 16:58:50 +0100

    make javascript work

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //document.getElementById("count-el").innerText = 10;
-let count = 5+7;
-console.log(count);
+
+//initialize count as 0
+//listen to the clicks
+//increment count with every click
+//change count-el in html to reflect new count
+
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+    
+}
+
