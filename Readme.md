d321b2287c0c18ecbb2f60a723d1d2cb1fd58fa0
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2022-08-15 19:15:21 +0100

    complete app and added readme file.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# A passenger counter app with JavaScript
+Simple subway passenger counter app.
+- It counts how many passenger entered the station.
+- Once you click save, it logs the number of passengers to the screen and starts from 0 again. 
+- It shows all saved values.
--- script.js
+++ script.js
@@ -6,14 +6,29 @@
 //change count-el in html to reflect new count
 
 let countEl = document.getElementById("count-el");
-console.log(countEl);
+let saveEl = document.getElementById("save-el")
+let totalCount = document.getElementById("total-count")
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
-    console.log(count)
+    count += 1
+    countEl.textContent = count
     
 }
 
+// 1. Create a function, save(), which logs out the count when it's called
+function save(){
+    document.getElementById("save-btn").innerText(count)
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let currentCount = `${count} - `
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += currentCount
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+    count = 0
+    countEl.textContent = count
+}
